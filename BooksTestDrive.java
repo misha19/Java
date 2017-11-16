0be27c7aa8d07f4d9178533ff59617465fa57877
Mehran Barzegar <zdz4455@gmail.com>
2017-11-16 09:34:24 +0100

    book objects
    created

--- BooksTestDrive.java
+++ BooksTestDrive.java
@@ -11,6 +11,9 @@ public class BooksTestDrive {
 	
 	Books[] myBooks = new Books[3];
 	int x= 0 ; 
+	myBooks[0] = new Books();
+	myBooks[1] = new Books(); 
+	myBooks[2] = new Books();
 	
 	myBooks[0].title = "The Grapes of Java"; 
 	myBooks[1].title= "The Java guide to the galaxy";
