48143b331a4f1e27e2ebe7018dc18d060c63bad9
Mehran Barzegar <zdz4455@gmail.com>
2017-11-24 18:45:53 +0100

    Add files via upload

---
+++ FirstDialog.java
@@ -0,0 +1,13 @@
+package javaprogramming;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "The first Java dialog");
+
+	}
+
+}
