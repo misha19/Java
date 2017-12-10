5b27bafab871d22b5d833d5a1a48d41128c193ce
Mehran Barzegar <zdz4455@gmail.com>
2017-12-10 14:26:58 +0100

    JavaFX

---
+++ Main.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(20);
+        root.setHgap(20);
+        Label greeting= new Label("You take the red or the blue pill?");
+        root.getChildren().add(greeting);
+        greeting.setTextFill(Color.LIGHTCORAL);
+        greeting.setFont(Font.font("Times New Roman ", FontWeight.BOLD,42 ));
+
+        primaryStage.setTitle("Welcome to The real world");
+        primaryStage.setScene(new Scene(root, 800, 675));
+
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
