2407ac7b91bc5dd510decd90184d98635fa22eb3
Mehran Barzegar <zdz4455@gmail.com>
2017-12-25 13:24:19 +0100

    break continue

--- printer.java
+++ printer.java
@@ -1,9 +1,12 @@
 /**
- * Created by me on 2017-12-21.
+ * Created by Mehran Barzegar on 2017-12-21.
+ * pluralsight tutorial
  */
 public class printer {
-    public boolean isOn;
-    public String modelNumber;
+   //access modifier
+
+    private boolean isOn;
+    private String modelNumber;
 
 //
 //    public void print() {
@@ -15,12 +18,75 @@ public class printer {
         this.isOn = isOn;
         this.modelNumber = modelNumber;
     }
-    public void print(){
-        System.out.println(isOn);
-        System.out.println(modelNumber);
+    public void print(int copies){
+     //   System.out.println(isOn);
+      //  System.out.println(modelNumber);
+      //  print("I can call a private method from within another method");
+
+        String onStatus = "";
+        if(isOn)
+            onStatus = "is On!";
+                    else
+                        onStatus = "is Off!";
+        String textToPrint = modelNumber + onStatus;
+
+        //for ( int i=0;i < copies ; i++ ){
+            while (copies>0) {
+                System.out.println(textToPrint);
+               copies--;
+            }
+//            if(isOn){
+//                System.out.println(modelNumber+ " is on!");
+//            }
+//            else {
+//                System.out.println(modelNumber+ " is off!");
+//            }
+
+
+        }
+//        switch (copies)
+//        {
+//            case 0 :
+//                break;
+//            case 1:
+//                if(isOn){
+//                    System.out.print(modelNumber+ " is on!");
+//                }
+//                else {
+//                    System.out.print(modelNumber+ " is off!");
+//                }
+//                break;
+//            case 2:
+//                if(isOn){
+//                    System.out.print(modelNumber+ " is on!");
+//                    System.out.print(modelNumber+ " is on!");
+//                }
+//                else {
+//                    System.out.print(modelNumber+ " is off!");
+//                    System.out.print(modelNumber+ " is off!");
+//
+//                }
+//                break;
+//            default:
+//                System.out.print("Number of copies are more that the " +
+//                        "number that can be printed!");
+//        }
+
+
+          //  }
+
+    public void printColors() {
+        String[] colors = new String[] {"Red","Blue","Green","yellow","Cyan"};
+        for (String currentColor : colors){
+
+            if("Green".equals(currentColor))
+                break;
+         
+            System.out.println(currentColor);
+        }
     }
 
-    public void print(String text){
+    private void print(String text){
         System.out.println(text);
     }
     public String getModelNumber() {
