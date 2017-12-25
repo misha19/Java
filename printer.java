06bbdbeda37c881e0401491ef8d671f0e0de2fde
Mehran Barzegar <zdz4455@gmail.com>
2017-12-25 10:33:14 +0100

    for loop
    first version

--- printer.java
+++ printer.java
@@ -1,9 +1,12 @@
 /**
- * Created by me on 2017-12-21.
+ * Created by Mehran Barzegar on 2017-12-21.
+ * pluralsight tutorial
  */
 public class printer {
-    public boolean isOn;
-    public String modelNumber;
+   //access modifier
+
+    private boolean isOn;
+    private String modelNumber;
 
 //
 //    public void print() {
@@ -15,12 +18,53 @@ public class printer {
         this.isOn = isOn;
         this.modelNumber = modelNumber;
     }
-    public void print(){
-        System.out.println(isOn);
-        System.out.println(modelNumber);
-    }
+    public void print(int copies){
+     //   System.out.println(isOn);
+      //  System.out.println(modelNumber);
+      //  print("I can call a private method from within another method");
 
-    public void print(String text){
+        for ( int i=0;i < copies ; i++ ){
+
+            if(isOn){
+                System.out.println(modelNumber+ " is on!");
+            }
+            else {
+                System.out.println(modelNumber+ " is off!");
+            }
+
+        }
+//        switch (copies)
+//        {
+//            case 0 :
+//                break;
+//            case 1:
+//                if(isOn){
+//                    System.out.print(modelNumber+ " is on!");
+//                }
+//                else {
+//                    System.out.print(modelNumber+ " is off!");
+//                }
+//                break;
+//            case 2:
+//                if(isOn){
+//                    System.out.print(modelNumber+ " is on!");
+//                    System.out.print(modelNumber+ " is on!");
+//                }
+//                else {
+//                    System.out.print(modelNumber+ " is off!");
+//                    System.out.print(modelNumber+ " is off!");
+//
+//                }
+//                break;
+//            default:
+//                System.out.print("Number of copies are more that the " +
+//                        "number that can be printed!");
+//        }
+
+
+            }
+
+    private void print(String text){
         System.out.println(text);
     }
     public String getModelNumber() {
