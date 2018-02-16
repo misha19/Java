94b0b962902dc7a4405541e3aec1f56df4ccba87
Mehran Barzegar <zdz4455@gmail.com>
2018-02-16 23:23:00 +0100

    udemy
    variables

---
+++ console.java
@@ -0,0 +1,17 @@
+public class console {
+    public static void main(String[] args){
+        System.out.println("My name is Mehran!");
+
+        int myFirstNumber = 567/45;
+        int mySecondNumber = 45;
+        int myThirdNumber = 34;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int result = 1000 - myTotal ;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(result);
+    }
+}
