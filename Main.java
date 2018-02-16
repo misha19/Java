8b687153a0f8461ee7a1e58de7fc0f7e5048c914
Mehran Barzegar <zdz4455@gmail.com>
2018-02-16 23:51:38 +0100

    udemy
    variables02

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.MehranBarzegar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+	// challenge
+
+        byte var01 = 43;
+        short var02 = 25 ;
+        int var03 = 46324;
+        long var04 = 54329344L ;
+
+        long result = 50000L + 10L * (var01 + var02 + var03 + var04) ;
+
+        System.out.println(result);
+
+    }
+}
